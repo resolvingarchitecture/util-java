591e2a264d830f722b875fa2d30c6fefd4f4da6f
objectorange <brian@resolvingarchitecture.io>
2020-06-23 08:47:23 -0700

    add randomAlphanumeric

--- src/main/java/ra/util/RandomUtil.java
+++ src/main/java/ra/util/RandomUtil.java
@@ -1,5 +1,6 @@
 package ra.util;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtil {
@@ -18,8 +19,17 @@ public class RandomUtil {
     public static int nextRandomInteger(int upperBound) {
         return ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, upperBound);
     }
-
     public static int nextRandomInteger(int lowerBound, int upperBound) {
         return ThreadLocalRandom.current().nextInt(lowerBound, upperBound);
+    }
+    public static String randomAlphanumeric(int length) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
